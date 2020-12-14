d1fee5fcf883acbc9aba07bce11ed1a22a6fbe00
Stephen Lewis <stephenrlewis@google.com>
2020-12-14 14:11:52 -0800

    Updated references to old terraform-plugin-sdk

--- converters/google/convert.go
+++ converters/google/convert.go
@@ -20,7 +20,7 @@ import (
 	"sort"
 	"time"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/pkg/errors"
 	provider "github.com/hashicorp/terraform-provider-google/v3/google"
 
--- tfplan/plan.go
+++ tfplan/plan.go
@@ -17,8 +17,8 @@ package tfplan
 import (
 	"strings"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 // Resource is the terraform representation of a resource.
