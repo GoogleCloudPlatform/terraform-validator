e33d46b91e74832c884b44d854340252ee926560
Stephen Lewis <stephenrlewis@google.com>
2020-12-17 13:27:15 -0800

    Bumped test provider version to 3.51.0

--- test/init_test.go
+++ test/init_test.go
@@ -20,7 +20,7 @@ const (
 	defaultOrganization    = "12345"
 	defaultFolder          = "67890"
 	defaultProject         = "foobar"
-	defaultProviderVersion = "3.0.0"
+	defaultProviderVersion = "3.51.0"
 )
 
 var (
