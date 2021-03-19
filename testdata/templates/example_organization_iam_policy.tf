3578fba41180bc1e8705097f4b06c382b0727f7c
Stephen Lewis <stephenrlewis@google.com>
2021-03-19 08:47:11 -0700

    Removed data sources from tf examples
    It turns out that running tests with -refresh=false that use data sources to generate json doesn't work in tf 0.12

--- testdata/templates/example_organization_iam_policy.tf
+++ testdata/templates/example_organization_iam_policy.tf
@@ -29,15 +29,5 @@ provider "google" {
 
 resource "google_organization_iam_policy" "policy" {
   org_id = "123456789"
-  policy_data = data.google_iam_policy.admin.policy_data
-}
-
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/editor"
-
-    members = [
-      "user:jane@example.com",
-    ]
-  }
+  policy_data = "{\"bindings\":[{\"members\":[\"user:jane@example.com\"],\"role\":\"roles/editor\"}]}"
 }
