b13fbf9bad853ea7b5c774cf76aff8cb1285b4b8
Cameron Thornton <camthornton@google.com>
2021-05-20 16:00:46 -0500

    fix correct strings lib

--- converters/google/convert.go
+++ converters/google/convert.go
@@ -260,7 +260,7 @@ func (c *Converter) addDelete(rc *tfjson.ResourceChange) error {
 			} else if !c.offline {
 				asset, err := mapper.fetch(&rd, c.cfg)
 				if err != nil {
-					if string.Contains(err.Error(), "Did not return a value") {
+					if strings.Contains(err.Error(), "Did not return a value") {
 						glog.Warningf("%s did not return a value for ID field. Skipping asset.", key)
 						continue
 					}
@@ -311,6 +311,10 @@ func (c *Converter) addCreateOrUpdate(rc *tfjson.ResourceChange) error {
 			} else if mapper.fetch != nil && !c.offline {
 				asset, err := mapper.fetch(&rd, c.cfg)
 				if err != nil {
+					if strings.Contains(err.Error(), "Did not return a value") {
+						glog.Warningf("%s did not return a value for ID field. Skipping asset.", key)
+						continue
+					}
 					return errors.Wrap(err, "fetching asset")
 				}
 
