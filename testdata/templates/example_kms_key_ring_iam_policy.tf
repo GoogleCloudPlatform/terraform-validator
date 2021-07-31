4ff73e81bbf7142706a4168a06d9838d3e8fcb1b
Palani Ram <palani.ram@googlecloud.corp-partner.google.com>
2021-07-31 11:03:35 +0530

    add all new validator test files includes, kms iam and kms key ring test data (#1)
    * add kms keyring and kms key iam resource support
    
    * Added templates for kms Iam Cryptokeys and keyrings
    
    * example-kms-iam added
    
    * Formatted all kms test
    
    * add example test cases to test go files
    
    * add json files for kms iam testing
    
    * fixing the conflicts for kms iam json test files
    
    * deleted mappers go
    
    * add kms keyring and kms key iam resource support
    
    * add kms keyring and kms crypto key validators
    
    * Added templates for kms Iam Cryptokeys and keyrings
    
    * example-kms-iam added
    
    * Formatted all kms test
    
    * add example test cases to test go files
    
    * add json files for kms iam testing
    
    * fixing the conflicts for kms iam json test files
    
    * rebasing with parent master
    
    * add kms keyring and kms key iam resource support
    
    * add kms keyring and kms key iam resource support
    
    * Added templates for kms Iam Cryptokeys and keyrings
    
    * example-kms-iam added
    
    * Formatted all kms test
    
    * add example test cases to test go files
    
    * add json files for kms iam testing
    
    * fixing the conflicts for kms iam json test files
    
    * deleted mappers go
    
    * add kms keyring and kms key iam resource support
    
    * add kms keyring and kms crypto key validators
    
    * Added templates for kms Iam Cryptokeys and keyrings
    
    * example-kms-iam added
    
    * Formatted all kms test
    
    * add example test cases to test go files
    
    * add json files for kms iam testing
    
    * fixing the conflicts for kms iam json test files
    
    * rebasing with parent master
    
    * add kms keyring and kms key iam resource support
    
    * add kms keyring and kms key iam resource support
    
    * Added templates for kms Iam Cryptokeys and keyrings
    
    * example-kms-iam added
    
    * Formatted all kms test
    
    * add example test cases to test go files
    
    * add json files for kms iam testing
    
    * fixing the conflicts for kms iam json test files
    
    * deleted mappers go
    
    * add kms keyring and kms key iam resource support
    
    * add kms keyring and kms crypto key validators
    
    * Added templates for kms Iam Cryptokeys and keyrings
    
    * example-kms-iam added
    
    * Formatted all kms test
    
    * add example test cases to test go files
    
    * add json files for kms iam testing
    
    * fixing the conflicts for kms iam json test files
    
    * add kms keyring and kms key iam resource support
    
    * Added templates for kms Iam Cryptokeys and keyrings
    
    * example-kms-iam added
    
    * Formatted all kms test
    
    * add example test cases to test go files
    
    * add json files for kms iam testing
    
    * fixing the conflicts for kms iam json test files
    
    * deleted mappers go
    
    * add kms keyring and kms key iam resource support
    
    * Added templates for kms Iam Cryptokeys and keyrings
    
    * example-kms-iam added
    
    * Formatted all kms test
    
    * add example test cases to test go files
    
    * add json files for kms iam testing
    
    * fixing the conflicts for kms iam json test files
    
    * mappers deleted from parent branch
    
    * add replace to google conversion
    
    * adding kms iam files while resolving conflicts
    
    * rebased
    
    * example-kms-iam added
    
    * Formatted all kms test
    
    * add example test cases to test go files
    
    * resolved conflicts
    
    * fixing the conflicts for kms iam json test files
    
    * add kms keyring and kms key iam resource support
    
    * example-kms-iam added
    
    * Formatted all kms test
    
    * add example test cases to test go files
    
    * add json files for kms iam testing
    
    * fixing the conflicts for kms iam json test files
    
    * add example test cases to test go files
    
    * Please enter the commit message for your changes. Lines starting
    
    * removing mappers file as it was moved to magic modules and updating google converion version
    
    * remove google conversion local reference
    
    Co-authored-by: Palani Ram <palani@palani-ram.lan>

---
+++ testdata/templates/example_kms_key_ring_iam_policy.tf
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> {{.Provider.version}}"
+    }
+  }
+}
+
+provider "google" {
+  {{if .Provider.credentials }}credentials = "{{.Provider.credentials}}"{{end}}
+}
+
+data "google_kms_key_ring" "kms-keyring-dev" {
+  name = "kms-keyring-dev"
+  location = "global"
+}
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/editor"
+    members = [
+      "allUsers", "allAuthenticatedUsers"
+    ]
+  }
+}
+
+resource "google_kms_key_ring_iam_policy" "key_ring" {
+  key_ring_id = data.google_kms_key_ring.kms-keyring-dev.id 
+  policy_data = data.google_iam_policy.admin.policy_data
+}
