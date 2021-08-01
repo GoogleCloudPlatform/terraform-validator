04c3ce1470004cf6be1d762b8bffb3845a55ed09
Palani Ram <palani.ram@googlecloud.corp-partner.google.com>
2021-08-01 05:18:25 -0400

    Update Google Conversion version to use KMS IAM validators and add kms iam tests

--- testdata/templates/example_kms_key_ring_iam_policy.tf
+++ testdata/templates/example_kms_key_ring_iam_policy.tf
@@ -27,19 +27,17 @@ provider "google" {
   {{if .Provider.credentials }}credentials = "{{.Provider.credentials}}"{{end}}
 }
 
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/editor"
+    members = [
+      "allUsers", "allAuthenticatedUsers"
+    ]
+  }
+}
+
 resource "google_kms_key_ring_iam_policy" "key_ring" {
   key_ring_id = "{{.Provider.project}}/global/keyring-example"
-  policy_data = jsonencode(
-    {
-      bindings = [
-        {
-          members = [
-            "allAuthenticatedUsers",
-            "serviceAccount:998476993360@cloudservices.gserviceaccount.com",
-          ]
-          role = "roles/cloudkms.admin"
-        }
-      ]
-    }
-  )
+  
+  policy_data = data.google_iam_policy.admin.policy_data
 }
