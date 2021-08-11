92285a72e71f94e964859c37b4cbfb3ee4b08950
Stephen Lewis <stephenrlewis@google.com>
2021-08-11 09:30:35 -0700

    Bundled third party notices and source code with executable for release

--- release.sh
+++ release.sh
@@ -10,20 +10,35 @@ fi
 
 version=$1
 
-if ! echo $version | grep -Eq "^v[0-9]+\.[0-9]+\.[0-9]+$"
+if ! echo $version | grep -Eq "^[0-9]+\.[0-9]+\.[0-9]+$"
 then
 	echo "Invalid version: ${version}"
-	echo "Please specify a semantic version prefixed with a v (e.g. v0.0.0)."
+	echo "Please specify a semantic version with no prefix (e.g. X.X.X)."
 	exit 1
 fi
+
+release_dir="./release/${version}"
+rm -rf $release_dir
+mkdir -p $release_dir
+
+echo "Go version: $(go version)"
+go get github.com/google/go-licenses
+echo "Downloading licenses and source code to bundle..."
+# Ignore errors until https://github.com/google/go-licenses/pull/77 is merged
+set +e
+go-licenses save "github.com/GoogleCloudPlatform/terraform-validator" --save_path="./${release_dir}/THIRD_PARTY_NOTICES"
+set -e
+echo "Zipping licenses and source code..."
+pushd "${release_dir}" > /dev/null
+zip -rq9D "THIRD_PARTY_NOTICES.zip" "THIRD_PARTY_NOTICES/"
+popd > /dev/null
 
 architectures="amd64 arm64"
 platforms="linux windows darwin"
 skip_platform_arch_pairs=" windows/arm64 "
 
-build_dir=./bin
-name=terraform-validator
-ldflags="-X github.com/GoogleCloudPlatform/terraform-validator/tfgcv.buildVersion=${version}"
+binary_name=terraform-validator
+ldflags="-X github.com/GoogleCloudPlatform/terraform-validator/tfgcv.buildVersion=v${version}"
 release_bucket=terraform-validator
 
 # Build release versions
@@ -34,10 +49,12 @@ for platform in ${platforms}; do
 			continue
 		fi
 
-
-		echo "Building version ${version} for platform ${platform} / arch ${arch}..."
-		GO111MODULE=on GOOS=${platform} GOARCH=${arch} CGO_ENABLED=0 go build -ldflags "${ldflags}" -o "${build_dir}/${name}-${platform}-${arch}" .
-		echo "Done building version ${version} for platform ${platform} / arch ${arch}"
+		echo "Building ${binary_name} v${version} for platform ${platform} / arch ${arch}..."
+		GO111MODULE=on GOOS=${platform} GOARCH=${arch} CGO_ENABLED=0 go build -ldflags "${ldflags}" -o "${release_dir}/${binary_name}" .
+		echo "Creating ${release_dir}/${binary_name}_${platform}_${arch}-${version}.tar.gz"
+		pushd "${release_dir}" > /dev/null
+		tar -czf "${binary_name}_${platform}_${arch}-${version}.tar.gz" "${binary_name}" "THIRD_PARTY_NOTICES.zip"
+		popd > /dev/null
 	done
 done
 
@@ -49,7 +66,7 @@ echo "Github tag ${version} created"
 # Publish release versions
 echo "Pushing releases to Google Storage"
 for arch in ${architectures}; do
-	gsutil cp ${build_dir}/*-${arch} gs://${release_bucket}/releases/${version}
+	gsutil cp ${release_dir}/*.tar.gz gs://${release_bucket}/releases/v${version}
 done
 echo "Releases pushed to Google Storage"
 
