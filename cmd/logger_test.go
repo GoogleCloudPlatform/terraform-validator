82f408d319e3a44df0e4ffed287e3ee18ea80887
Stephen Lewis <stephenrlewis@google.com>
2021-09-24 08:38:40 -0700

    Added test of outputLogger formatting

--- cmd/logger_test.go
+++ cmd/logger_test.go
@@ -76,3 +76,47 @@ func TestErrorLoggerSchema(t *testing.T) {
 	_, err := time.Parse(time.RFC3339Nano, output["timestamp"].(string))
 	a.Nil(err)
 }
+
+func TestOutputLoggerSchema(t *testing.T) {
+	// Expected schema is:
+	// {
+	//     "version": "vX.X.X",
+	//     "timestamp": "RFC 3339-encoded timestamp",
+	//     "body": "Message content",  // Optional
+	//     "resource_body": {},  // Optional, {} or []
+	// }
+
+	resourceBody := []interface{}{"foo", "bar", "baz"}
+	outputLogger, buf := newTestOutputLogger()
+	outputLogger.Info(
+		"This is a message",
+		zap.Any("resource_body", resourceBody),
+	)
+
+	outputJSON := buf.Bytes()
+
+	var output map[string]interface{}
+	json.Unmarshal(outputJSON, &output)
+
+	expectedOutput := map[string]interface{}{
+		"version":       "v1.0.0",
+		"timestamp":     "tested separately",
+		"body":          "This is a message",
+		"resource_body": resourceBody,
+	}
+
+	a := assert.New(t)
+	a.Equal(len(output), len(expectedOutput))
+
+	for k := range expectedOutput {
+		a.Contains(output, k)
+	}
+
+	a.Equal(output["version"], expectedOutput["version"])
+	a.Equal(output["body"], expectedOutput["body"])
+	a.Equal(output["resource_body"], expectedOutput["resource_body"])
+
+	// This should not fail
+	_, err := time.Parse(time.RFC3339Nano, output["timestamp"].(string))
+	a.Nil(err)
+}
