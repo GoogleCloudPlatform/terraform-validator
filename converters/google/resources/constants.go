647a4f396af99db143d367f4b1e2e1be0360d7a0
Stephen Lewis <stephenrlewis@google.com>
2021-10-19 15:01:58 -0700

    Added google resources dir

---
+++ converters/google/resources/constants.go
@@ -0,0 +1,22 @@
+package google
+
+import (
+	"errors"
+)
+
+// ErrNoConversion can be returned if a conversion is unable to be returned
+// because of the current state of the system.
+// Example: The conversion requires that the resource has already been created
+// and is now being updated).
+var ErrNoConversion = errors.New("no conversion")
+
+// ErrEmptyIdentityField can be returned when fetching a resource is not possible
+// due to the identity field of that resource returning empty.
+var ErrEmptyIdentityField = errors.New("empty identity field")
+
+// ErrLackingReadPermissions can be returned when fetching a resource is not possible
+// due to the user not having read permissions.
+var ErrLackingReadPermission = errors.New("lacking read permissions")
+
+// Global MutexKV
+var mutexKV = NewMutexKV()
