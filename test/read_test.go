b2c59c700135de0fafee80fd8edc70ece2fe7415
Modular Magician <magic-modules@google.com>
2021-11-24 18:58:39 +0000

    Add support for google_compute_address (#5483)
    * Add support for google_compute_address
    
    * add test data files
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- test/read_test.go
+++ test/read_test.go
@@ -38,6 +38,7 @@ func TestReadPlannedAssetsCoverage(t *testing.T) {
 		{name: "example_bigtable_instance"},
 		{name: "example_cloud_run_mapping"},
 		{name: "example_cloud_run_service"},
+		{name: "example_compute_address"},
 		{name: "example_compute_disk"},
 		{name: "example_compute_disk_empty_image"},
 		{name: "example_compute_firewall"},
