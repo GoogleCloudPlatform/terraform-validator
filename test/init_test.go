81053fc029a5c59fa8a452ad1af0d0030acf4cf5
Iris Chen <10179943+iyabchen@users.noreply.github.com>
2021-12-10 18:31:56 -0500

    Up version provider test version to 4.3.0 (#429)

--- test/init_test.go
+++ test/init_test.go
@@ -21,7 +21,7 @@ const (
 	defaultOrganization    = "12345"
 	defaultFolder          = "67890"
 	defaultProject         = "foobar"
-	defaultProviderVersion = "3.84.0"
+	defaultProviderVersion = "4.3.0"
 )
 
 var (
