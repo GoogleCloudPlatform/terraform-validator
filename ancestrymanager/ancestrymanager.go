cae99c2b34ad77da7e1f905b2ab1f6e753318c29
Zhenhua Li <zhenhuali@google.com>
2023-03-01 09:55:10 -0800

    Use the new getter method for config.userAgent (#1408)

--- ancestrymanager/ancestrymanager.go
+++ ancestrymanager/ancestrymanager.go
@@ -48,9 +48,9 @@ func New(cfg *resources.Config, offline bool, entries map[string]string, errorLo
 		errorLogger:   errorLogger,
 	}
 	if !offline {
-		am.resourceManagerV1 = cfg.NewResourceManagerClient(cfg.UserAgent())
-		am.resourceManagerV3 = cfg.NewResourceManagerV3Client(cfg.UserAgent())
-		am.storageClient = cfg.NewStorageClient(cfg.UserAgent())
+		am.resourceManagerV1 = cfg.NewResourceManagerClient(cfg.GetUserAgent())
+		am.resourceManagerV3 = cfg.NewResourceManagerV3Client(cfg.GetUserAgent())
+		am.storageClient = cfg.NewStorageClient(cfg.GetUserAgent())
 	}
 	err := am.initAncestryCache(entries)
 	if err != nil {
