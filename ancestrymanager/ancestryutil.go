b6fa9c2670fca5ac717bb48bd147855845bc78d6
jashan-g <98514022+jashan-g@users.noreply.github.com>
2023-03-21 23:17:48 +0530

    Update ancestryutil.go

--- ancestrymanager/ancestryutil.go
+++ ancestrymanager/ancestryutil.go
@@ -79,10 +79,6 @@ func getProjectFromSchema(projectSchemaField string, d resources.TerraformResour
 	if ok && projectSchemaField != "" {
 		return res.(string), nil
 	}
-	res, ok = d.GetOk("parent")
-	if ok && strings.Contains(res.(string), "projects/"){
-		return res.(string), nil
-	}
 	if config.Project != "" {
 		return config.Project, nil
 	}
@@ -93,10 +89,6 @@ func getProjectFromSchema(projectSchemaField string, d resources.TerraformResour
 func getOrganizationFromResource(tfData resources.TerraformResourceData) (string, bool) {
 	orgID, ok := tfData.GetOk("org_id")
 	if ok {
-		return orgID.(string), ok
-	}
-	orgID, ok = tfData.GetOk("parent")
-	if ok && strings.Contains(orgID.(string), "organizations/"){
 		return orgID.(string), ok
 	}
 	return "", false
@@ -110,10 +102,6 @@ func getFolderFromResource(tfData resources.TerraformResourceData) (string, bool
 	}
 	folderID, ok = tfData.GetOk("folder")
 	if ok {
-		return folderID.(string), ok
-	}
-	folderID, ok = tfData.GetOk("parent")
-	if ok && strings.Contains(folderID.(string), "folders/"){
 		return folderID.(string), ok
 	}
 	return "", false
