fd4e7cb913668544bb1799ec99f1089b8dd98672
Modular Magician <magic-modules@google.com>
2023-04-05 15:39:12 +0000

    Implement tgc code generation and test in ci (#7462)
    * chore(ci): enable tgc code engeration and test
    
    * skip build and test for tgc at downstream script
    
    * fix test param
    
    * remove code based on comment
    
    * fix local path to tfplan2cai
    
    * fix find and rm
    
    * use new ci file
    
    * Fix tgc local path
    
    * fix example_org_policy_policy
    
    * clean up unnecessary change
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- test/read_test.go
+++ test/read_test.go
@@ -80,6 +80,7 @@ func TestReadPlannedAssetsCoverage(t *testing.T) {
 		{name: "example_kms_key_ring_iam_policy"},
 		{name: "example_logging_metric"},
 		{name: "example_monitoring_notification_channel"},
+		{name: "example_org_policy_policy"},
 		{name: "example_organization_iam_binding"},
 		{name: "example_organization_iam_custom_role"},
 		{name: "example_organization_iam_member"},
